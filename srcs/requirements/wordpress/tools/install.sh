1f37b8640d5145b29a9bf04b79e9459380b90274
Theo <theo.duprez.mathieu@gmail.com>
2024-03-18 10:15:18 -0400

    inception almost finished

--- srcs/requirements/wordpress/tools/install.sh
+++ srcs/requirements/wordpress/tools/install.sh
@@ -2,9 +2,19 @@
 
 sleep 3
 
-wp-cli.phar config create --dbname=${WORDPRESS_DATABASE} --dbuser=${WORDPRESS_DATABASE_USER} --dbpass=${WORDPRESS_DATABASE_USER_PASSWORD} --dbhost=mariadb --path=/var/www/wordpress --allow-root
+if [ ! -d "/var/www/wordpress" ]; then
 
-wp-cli.phar core install --url="${URL}" --title="${TITLE}" --admin_user="${ADMIN_USER}" --admin_password="${ADMIN_PASSWORD}" --admin_email="theo.duprez.mathieu@gmail.com" --allow-root --path=/var/www/wordpress
+	wp-cli.phar core download --path=/var/www/wordpress --allow-root
+
+	chown -R www-data:www-data /var/www/wordpress
+
+	wp-cli.phar config create --dbname=${WORDPRESS_DATABASE} --dbuser=${WORDPRESS_DATABASE_USER} --dbpass=${WORDPRESS_DATABASE_USER_PASSWORD} --dbhost=mariadb --path=/var/www/wordpress --allow-root
+
+	wp-cli.phar core install --url="${WORDPRESS_WEBSITE_URL}" --title="${WORDPRESS_WEBSITE_TITLE}" --admin_user="${WORDPRESS_WEBSITE_ADMIN}" --admin_password="${WORDPRESS_WEBSITE_ADMIN_PASSWORD}" --admin_email=${WORDPRESS_WEBSITE_ADMIN_EMAIL} --allow-root --path=/var/www/wordpress
+
+	wp-cli.phar user create ${WORDPRESS_WEBSITE_USER} ${WORDPRESS_WEBSITE_USER_EMAIL} --user_pass=${WORDPRESS_WEBSITE_USER_PASSWORD} --role=editor --allow-root --path=/var/www/wordpress
+
+fi
 
 mkdir -p /run/php
 
