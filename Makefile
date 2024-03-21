0e6e849cd914ea45c90e5ca18acbd14e3cc5e9a0
Theo <theo.duprez.mathieu@gmail.com>
2024-03-21 14:52:19 -0400

    makefile is finished, need to clarify doc of wordpress and nginx

--- Makefile
+++ Makefile
@@ -1,13 +1,20 @@
 VOLUME_DATA="/home/tduprez/data"
+COMPOSE_PATH="srcs/docker-compose.yml"
 
 all:
-	mkdir -p $(VOLUME_DATA)/wordpress $(VOLUME_DATA)/mariadb
-	docker compose -f srcs/docker-compose.yml up -d
+	@mkdir -p $(VOLUME_DATA)/wordpress $(VOLUME_DATA)/mariadb
+	@docker compose -f $(COMPOSE_PATH) up --build -d
 
 down:
-	docker compose -f srcs/docker-compose.yml down
+	@docker compose -f $(COMPOSE_PATH) down
 
-fclean: down
-	sudo rm -rf $(VOLUME_DATA)
+clean:
+	@docker compose -f $(COMPOSE_PATH) down -v --rmi all
+
+fclean: clean
+	@docker system prune --all --force
+	@sudo rm -rf $(VOLUME_DATA)
 
 re: fclean all
+
+.PHONY: all down clean fclean re
--- srcs/requirements/wordpress/tools/install.sh
+++ srcs/requirements/wordpress/tools/install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+sleep 5
+
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
 
 	wp-cli.phar core download --path=/var/www/wordpress --allow-root
 
 	chown -R www-data:www-data /var/www/wordpress
 
-	wp-cli.phar config create --dbname=${WORDPRESS_DATABASE} --dbuser=${WORDPRESS_DATABASE_USER} --dbpass=${WORDPRESS_DATABASE_USER_PASSWORD} --dbhost=mariadb --path=/var/www/wordpress --allow-root
+	wp-cli.phar config create --dbname=${WORDPRESS_DATABASE} --dbuser=${WORDPRESS_DATABASE_USER} --dbpass=${WORDPRESS_DATABASE_USER_PASSWORD} --dbhost=mariadb:3306 --path=/var/www/wordpress --allow-root
 
 	wp-cli.phar core install --url="${WORDPRESS_WEBSITE_URL}" --title="${WORDPRESS_WEBSITE_TITLE}" --admin_user="${WORDPRESS_WEBSITE_ADMIN}" --admin_password="${WORDPRESS_WEBSITE_ADMIN_PASSWORD}" --admin_email=${WORDPRESS_WEBSITE_ADMIN_EMAIL} --allow-root --path=/var/www/wordpress
 
